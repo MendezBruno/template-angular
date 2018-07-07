1e1fbbd53241619f666817e7c59c0f0b5fbffbed
Fernando688 <40301656+Fernando688@users.noreply.github.com>
2018-07-07 19:20:37 -0300

    Se agrego el formulario con geosearch

--- src/app/vistas/forms/user-google-maps/user-google-maps.component.ts
+++ src/app/vistas/forms/user-google-maps/user-google-maps.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EsriProvider } from 'leaflet-geosearch';
+
+declare let L;   
+
 
 @Component({
   selector: 'app-user-google-maps',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserGoogleMapsComponent implements OnInit {
 
+  nuevoUsuario: any = {};
+  map: any;
+  ultimoMarker: any;
+  provider = new EsriProvider();  
+  
+
   constructor() { }
 
   ngOnInit() {
+
+    this.map = L.map('mapid').setView([-34.603722, -58.381592], 12);    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);    
+
+    document.querySelector('button').addEventListener('click', async (event) => {
+      event.preventDefault();
+  
+      var results = await this.provider.search({ query: this.nuevoUsuario.direccion });    
+      
+      this.nuevoUsuario.direccionValida = results.length > 0;
+
+      if(this.ultimoMarker!= null)
+          this.map.removeLayer(this.ultimoMarker);
+
+      if(!(this.nuevoUsuario.direccionValida))
+      {
+        alert('La dirección no es valida');        
+      }  
+      else
+      {
+        var x = results[0].x;
+        var y = results[0].y;       
+        
+        this.ultimoMarker = L.marker([y, x]);
+        
+        this.ultimoMarker.addTo(this.map)
+        .bindPopup(this.nuevoUsuario.direccion)
+        .openPopup();
+      }
+      
+    });    
+
+  }
+
+  guardarUsuario()
+  {
+    console.log(this.nuevoUsuario);
   }
 
 }
