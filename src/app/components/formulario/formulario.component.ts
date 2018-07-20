174f4b8a85b0a1ac516d2647c3b46aed4d3f1eeb
fdibenedetto <fdibenedetto@cideso.mil.ar>
2018-07-20 10:35:18 -0300

    se termino el formulario, se dejo comentado el item-uno por problemas de visualizacion

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -6,6 +6,16 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css'],
   animations: [
+    trigger('flyLeftToRigth', [
+      state('show', style({
+        transform: 'translateX(+100%)'
+      })),
+      state('hide',   style({
+        transform: 'translateX(-100%)'
+      })),
+      transition('show => hide', animate('1000ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+    ]),
     trigger('flyRigthToLeft', [
       state('show', style({
         transform: 'translateX(+100%)'
@@ -27,7 +37,8 @@ export class FormularioComponent implements OnInit {
 
   @HostListener('window:scroll', ['$event'])
     checkScroll() {
-      const componentPosition = this.el.nativeElement.offsetTop - 400;
+      
+      const componentPosition = this.el.nativeElement.getBoundingClientRect().top;
       const scrollPosition = window.pageYOffset;
 
       if (this.state == 'hide' && (scrollPosition >= componentPosition)) {
