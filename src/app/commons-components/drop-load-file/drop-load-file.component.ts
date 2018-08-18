28bad16d79b3c25b9aaab3136eb73a8a5ecbdc94
MendezBruno <brunomendez88@hotmail.com>
2018-08-18 18:46:10 -0300

    componente de insertar archivos terminado

--- src/app/commons-components/drop-load-file/drop-load-file.component.ts
+++ src/app/commons-components/drop-load-file/drop-load-file.component.ts
@@ -8,10 +8,16 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 export class DropLoadFileComponent implements OnInit {
 
   @ViewChild('panelDrop') panelDrop: ElementRef;
+  dropZone;
 
-  constructor() { }
-
+  constructor() { 
+    // Setup the dnd listeners.
+  }
+  
   ngOnInit() {
+    this.dropZone = document.getElementById('drop_zone');
+    this.dropZone.addEventListener('dragover', this.handleDragOver, false);
+    this.dropZone.addEventListener('drop', this.handleFileSelect, false);
   }
 
   dispatch(event$) {
@@ -19,5 +25,34 @@ export class DropLoadFileComponent implements OnInit {
     console.log(event$);
     console.log(this.panelDrop);
   }
+
+  handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files; // FileList object.
+    console.log(files);
+  }
+
+  handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+
+  /*
+  
+   // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate.toLocaleDateString(), '</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+  
+  */
+
 
 }
