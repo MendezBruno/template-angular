e562eab1ad7530f64b7785466c590bfd255c7630
MB118088 <bruno.mendez@icbc.com.ar>
2019-01-16 18:11:20 -0300

    avance canvas

--- src/app/components/mono-canvas/mono-canvas.component.ts
+++ src/app/components/mono-canvas/mono-canvas.component.ts
@@ -12,23 +12,67 @@ export class MonoCanvasComponent implements OnInit, AfterViewInit {
 
   public ctx: CanvasRenderingContext2D;
 
-  wsize = '150';
-  hsize = '150';
+  
+  // Tamaño del Lienzo
+  wsize = 150;
+  hsize = 150;
+
+  //Margenes de los ejes de coordenadas
+  margenY = 20;
+  margenX = 20;
+
+  // Origne del eje de coordenadas
+  origenX = this.margenX; 
+  origenY = this.hsize - this.margenY;
 
   constructor() { }
 
   ngAfterViewInit() {
-    this.draw();
+    this.inicializarCanvas();
+    
   }
 
-  ngOnInit() {
-    this.draw();
-  }
-
-  draw() {
+  inicializarCanvas(): any {
     this.ctx = this.canvasRef.nativeElement.getContext('2d');
-    this.ctx.strokeRect(50, 50, 50, 50);
-    this.ctx.fillRect(25, 25, 100, 100);
+    // Eje X
+    this.drawLine(this.origenX , this.origenY , this.wsize , this.origenY);
+
+    // Eje y
+    this.drawLine(this.origenX , this.origenY , this.origenX , this.hsize - this.origenY);
   }
+
+  ngOnInit() { }
+
+  drawLine(startX, startY, endX, endY){
+ 
+    this.ctx.beginPath();
+ 
+    this.ctx.moveTo(startX,startY);
+ 
+    this.ctx.lineTo(endX,endY);
+ 
+    this.ctx.stroke()
+  }
+
+  setLineWidth( width:number ) {
+    //Indicar el grosor de la línea
+    this.ctx.lineWidth = width;
+  }
+
+  // comparador(fecha1,fecha2) {
+  //   let aFecha1, aFecha2;
+  //   let i: number = 0;
+  //   aFecha1 = fecha1.split('-')
+  //   aFecha2 = fecha2.split('-')
+
+  //   while (aFecha1.length < i ) {
+  //     if  (Number(aFecha1[i]) < Number(aFecha2[i])) return -1;
+  //     if  (Number(aFecha1[i]) > Number(aFecha2[i])) return 1;
+  //   }
+
+  //   return 0
+  // }
+
+
 
 }
